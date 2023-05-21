d78b4fd1069894aad38f96aecc7e7661ccefced7
marcin bebas <marcin.bebas@accenture.com>
2023-05-21 18:37:27 +0200

    refactoring

--- src/main/java/com/accenture/distancemeter/controller/DistanceController.java
+++ src/main/java/com/accenture/distancemeter/controller/DistanceController.java
@@ -1,7 +1,6 @@
 package com.accenture.distancemeter.controller;
 
 import com.accenture.distancemeter.dto.DistanceDTO;
-import com.accenture.distancemeter.service.CodeService;
 import com.accenture.distancemeter.service.DistanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
